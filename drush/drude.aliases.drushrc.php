513e63607bbb726802a6a7d609abe266e35c6016
Dave Kopecek <dave@aisle8.net>
2016-08-22 10:57:58 -0400

    Add placeholder drush alias

---
+++ drush/drude.aliases.drushrc.php
@@ -0,0 +1,11 @@
+<?php
+
+$aliases['local'] = array(
+  'root' => '/var/www/docroot',
+  'uri'  => 'editme.drude',
+);
+
+$aliases['prod'] = array(
+  'root' => '/var/www/docroot',
+  'uri'  => 'editme.com',
+);
